c8bd702e00c10ecd0ad0832cd3c810eee8843cca
Babak Daliran <babakdaliran@gmail.com>
2025-09-20 19:20:52 +0330

    part 94 commit

--- resources/views/frontend/auth/login.blade.php
+++ resources/views/frontend/auth/login.blade.php
@@ -11,12 +11,14 @@
                         <div class="section-title title-wide mb-1 no-after-title-wide">
                             <h2 class="font-weight-bold">ورود</h2>
                         </div>
-                        <form action="#">
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+                            @include('frontend.auth.layouts.error')
                             <div class="form-row-title">
                                 <h3> شماره موبایل</h3>
                             </div>
                             <div class="form-row with-icon">
-                                <input type="text" class="input-ui pr-2"
+                                <input type="text" class="input-ui pr-2" name="mobile"
                                        placeholder=" شماره موبایل خود را وارد نمایید">
                                 <i class="mdi mdi-account-circle-outline"></i>
                             </div>
@@ -24,20 +26,20 @@
                                 <h3>رمز عبور</h3>
                             </div>
                             <div class="form-row with-icon">
-                                <input type="password" class="input-ui pr-2"
+                                <input type="password" class="input-ui pr-2" name="password"
                                        placeholder="رمز عبور خود را وارد نمایید">
                                 <i class="mdi mdi-lock-open-variant-outline"></i>
                             </div>
                             <div class="form-row mt-2">
                                 <div class="custom-control custom-checkbox float-right mt-2">
-                                    <input type="checkbox" class="custom-control-input" id="customCheck3">
+                                    <input type="checkbox" class="custom-control-input" name="remember" id="customCheck3">
                                     <label class="custom-control-label" for="customCheck3">
                                         مرا به خاطر بسپار
                                     </label>
                                 </div>
                             </div>
                             <div class="form-row mt-3">
-                                <button class="btn-primary-cm btn-with-icon mx-auto w-100">
+                                <button type="submit" class="btn-primary-cm btn-with-icon mx-auto w-100">
                                     <i class="mdi mdi-login-variant"></i>
                                     ورود
                                 </button>
