c9e3fa1d2ec4c7ac7eb7aa9cb8fb6b6d936d4f53
babak-daliran <babakdaliran13@gmail.com>
2025-12-04 19:09:53 +0330

    profile user part-8 favorites list part-2 commit

--- app/Models/Favorite.php
+++ app/Models/Favorite.php
@@ -11,4 +11,13 @@ class Favorite extends Model
         'product_id',
 
     ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
